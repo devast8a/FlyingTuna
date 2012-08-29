4f2b829198d5c12649250f1a0491d5dfd568a129
devast8a <devast8a@whitefirex.com>
2012-08-29 14:39:11 +0930

    Dictionary Extension

---
+++ FlyingTuna/Utils/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlyingTuna.Utils.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
+        {
+            TValue value;
+            return dict.TryGetValue(key, out value) ? value : default(TValue);
+        }
+    }
+}
