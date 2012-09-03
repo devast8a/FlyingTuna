15ad051c02899a2492209fa229fe29c2ff5177b8
devast8a <devast8a@whitefirex.com>
2012-09-03 13:32:34 +0930

    Merged resource cache and texture utility class from BitterNeutron.

---
+++ FlyingTuna/Additions/ResourceCache.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FlyingTuna.Additions
+{
+    public class ResourceCache
+    {
+        class Inner
+        {
+            public readonly Dictionary<string, object> Values = new Dictionary<string, object>();
+            public Func<string,object> Loader;
+        }
+
+        private readonly Dictionary<Type, Inner> _resource = new Dictionary<Type,Inner>();
+
+        private Inner GetResourceCache(Type type)
+        {
+            Inner value;
+            if (!_resource.TryGetValue(type, out value))
+            {
+                value = new Inner();
+                _resource.Add(type, value);
+            }
+
+            return value;
+        }
+
+        public Func<string, object> GetLoader<T>()
+        {
+            return GetResourceCache(typeof(T)).Loader;
+        }
+
+        public void SetLoader<T>(Func<string, object> value)
+        {
+            GetResourceCache(typeof(T)).Loader = value;
+        }
+
+        public T Load<T>(string name)
+        {
+            var rescache = GetResourceCache(typeof(T));
+
+            object value;
+
+            if (!rescache.Values.TryGetValue(name, out value))
+            {
+                if (rescache.Loader == null)
+                {
+                    throw new Exception("Called load before setting loader");
+                }
+
+                value = rescache.Loader(name);
+
+                if(!(value is T))
+                {
+                    throw new Exception("Loaded resource is not of type " + typeof(T));
+                }
+
+                rescache.Values.Add(name, value);
+            }
+
+            return (T)value;
+        }
+
+        public void Store<T>(string name, T obj, bool overwrite = false)
+        {
+            var rescache = GetResourceCache(typeof(T));
+
+            if(overwrite)
+            {
+                if(rescache.Values.ContainsKey(name))
+                {
+                    throw new Exception("Already contains a resource with name " + name);
+                }
+            }
+
+            rescache.Values[name] = obj;
+        }
+    }
+}
---
+++ FlyingTuna/BN/TextureUtil.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FlyingTuna.BN
+{
+    public static class TextureUtil
+    {
+        public static Texture2D ErrorTexture;
+
+        // Returns the default texture to be used when one isn't found
+        private static Texture2D GetErrorTexture(GraphicsDevice graphicsDevice)
+        {
+            if(ErrorTexture != null)
+            {
+                return ErrorTexture;
+            }
+
+            const int width = 32;
+            const int height = 32;
+
+            // Colors for the texture
+            Color
+                topLeft = Color.DarkViolet,
+                topRight = Color.Teal,
+                bottomLeft = Color.Teal,
+                bottomRight = Color.DarkViolet;
+
+            ErrorTexture = new Texture2D(graphicsDevice, width, height);
+            var c = new Color[width * height];
+
+            // generate a texture with 4 squares
+            for (int x = 0; x < width / 2; x++ )
+            {
+                for(int y = 0 ; y < height / 2; y++)
+                {
+                    // XL = Left hand side coordinate
+                    // XR = Right hand side coordinate
+                    // YB = Bottom Coordinate
+                    var xl = x * height;
+                    var xr = (x + (width / 2)) * height;
+                    var yb = y + (height / 2);
+
+                    c[xl + y] = topLeft;
+                    c[xr + y] = topRight;
+                    c[xl + yb] = bottomLeft;
+                    c[xr + yb] = bottomRight;
+                }
+            }
+
+            ErrorTexture.SetData(c);
+
+            return ErrorTexture;
+        }
+
+        public static Texture2D GetTexture(string res, GraphicsDevice graphicsDevice)
+        {
+            if (File.Exists(res))
+            {
+                return Texture2D.FromStream(graphicsDevice, new FileStream(res, FileMode.Open, FileAccess.Read, FileShare.Read));
+            }
+
+            Console.WriteLine("Texture not found: " + Path.GetFullPath(res));
+
+            return GetErrorTexture(graphicsDevice);
+        }
+    }
+}
