9a940c780da644836aabc1c0cb99559cf5cbe942
devast8a <devast8a@whitefirex.com>
2012-09-09 10:21:11 +0930

    Extended Dictionary.GetOrDefault

--- FlyingTuna/Utils/Extensions/DictionaryExtensions.cs
+++ FlyingTuna/Utils/Extensions/DictionaryExtensions.cs
@@ -12,5 +12,11 @@ namespace FlyingTuna.Utils.Extensions
             TValue value;
             return dict.TryGetValue(key, out value) ? value : default(TValue);
         }
+
+        public static TValue GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue defaultValue)
+        {
+            TValue value;
+            return dict.TryGetValue(key, out value) ? value : defaultValue;
+        }
     }
 }
