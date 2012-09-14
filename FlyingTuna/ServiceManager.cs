e0338da227022c37fbfe171ed9b2107f1506bc14
devast8a <devast8a@whitefirex.com>
2012-09-14 14:07:25 +0930

    Service provider uses strings instead of just types.

--- FlyingTuna/ServiceManager.cs
+++ FlyingTuna/ServiceManager.cs
@@ -5,21 +5,36 @@ namespace FlyingTuna
 {
     public class ServiceManager
     {
-        private readonly Dictionary<Type, object> _services = new Dictionary<Type, object>();
-
+        private readonly Dictionary<string, object> _services = new Dictionary<string, object>();
+        
         public T GetProvider<T>()
         {
-            return (T)GetProvider(typeof(T));
+            return (T)GetProvider(typeof(T).ToString());
+        }
+
+        public T GetProvider<T>(string name)
+        {
+            return (T)GetProvider(name);
         }
 
         public object GetProvider(Type type)
         {
-            return _services[type];
+            return GetProvider(type.ToString());
+        }
+
+        private object GetProvider(string name)
+        {
+            return _services[name];
         }
 
         public void SetProvider<T>(T service)
         {
-            _services.Add(typeof(T), service);
+            SetProvider(typeof(T).ToString(), service);
+        }
+
+        public void SetProvider(string name, object service)
+        {
+            _services.Add(name, service);
         }
     }
 }
