eeb2867ab5b402c5b8fd2b12d7cedb9b6bcd1c37
devast8a <devast8a@whitefirex.com>
2012-10-03 10:20:25 +0930

    Add a basic image class to handle spritesheets easier.

---
+++ FlyingTuna/Graphics/Image.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FlyingTuna.Graphics
+{
+    public class Image
+    {
+        public readonly Texture2D Texture;
+        public readonly Rectangle Source;
+        public Color Color = Color.White;
+
+        public Image(Texture2D texture)
+        {
+            Texture = texture;
+            Source = new Rectangle(0, 0, Texture.Width, Texture.Height);
+        }
+
+        public Image(Texture2D texture, Rectangle source)
+        {
+            Texture = texture;
+            Source = source;
+        }
+
+        public Image Slice(int width, int height)
+        {
+            return Slice(new Rectangle(0, 0, width, height));
+        }
+
+        public Image Slice(int x, int y, int width, int height)
+        {
+            return Slice(new Rectangle(x, y, width, height));
+        }
+
+        public Image Slice(Rectangle rectangle)
+        {
+            return new Image(Texture, rectangle){Color = Color};
+        }
+
+        public void Draw(SpriteBatch spritebatch, Vector2 position)
+        {
+            spritebatch.Draw(Texture, position, Source, Color);
+        }
+    }
+}
